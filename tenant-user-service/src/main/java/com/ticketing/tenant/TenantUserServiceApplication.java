234a7bb3418ce88c440bd8283b71a176c480cd33
mmm-vaibhav <mmm.vaibhav26@gmail.com>
2026-04-13 03:43:53 +0530

    repo initialiation

---
+++ tenant-user-service/src/main/java/com/ticketing/tenant/TenantUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.tenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantUserServiceApplication.class, args);
+	}
+
+}
---
+++ tenant-user-service/src/test/java/com/ticketing/tenant/TenantUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing.tenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
